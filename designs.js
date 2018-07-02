5f21a180e1c0cfb13312f1a40e7a12e5914e6615
Carlos Hernández Gil <chernandezgil@gmail.com>
2018-07-02 17:57:21 +0200

    feat: Add grid functionality

--- designs.js
+++ designs.js
@@ -1,8 +1,15 @@
 // Select color input
 // Select size input
 
-// When size is submitted by the user, call makeGrid()
+//global variables
+let selectedColor = "#000000";
 
+
+/*
+*
+* makeGrid
+*
+*/
 function makeGrid(event) {
   // Your code goes here!
   event.preventDefault();
@@ -41,9 +48,68 @@ function makeGrid(event) {
 
   } //end for i
 
+}
 
+/*
+*
+* applyColorToCell
+*
+*/
+function applyColorToCell(event) {
+  //console.log(event);
+
+  //the cell has a color background, but the  color is not equal als the selectedColor
+  if ( event.target.hasAttribute("style") && rgbToHex(event.target.style.backgroundColor) !== selectedColor ) {
+
+    event.target.style.backgroundColor = selectedColor;
+    //console.log("inside &&: selected color:"+selectedColor + ", backgroundColor"+ event.target.style.backgroundColor);
+
+  } else if( event.target.hasAttribute("style") ) {
+    //console.log("inside else if");
+
+    event.target.removeAttribute("style");
+
+  } else {
+    //console.log("inside else");
+
+    event.target.style.backgroundColor = selectedColor;
+
+  }
 
 }
 
+/*
+*
+* selectColorFromColorPicker
+*
+*/
+function selectColorFromColorPicker(event){
+  //returns the color in #format
+  selectedColor = event.target.value;
+}
 
+/*
+*
+* rgbToHex
+*
+*/
+function rgbToHex(col)
+{
+    if(col.charAt(0)=='r')
+    {
+        col=col.replace('rgb(','').replace(')','').split(',');
+        var r=parseInt(col[0], 10).toString(16);
+        var g=parseInt(col[1], 10).toString(16);
+        var b=parseInt(col[2], 10).toString(16);
+        r=r.length==1?'0'+r:r; g=g.length==1?'0'+g:g; b=b.length==1?'0'+b:b;
+        var colHex='#'+r+g+b;
+        return colHex;
+    }
+}
+
+
+
+//Eventlisteners
 document.addEventListener('submit', makeGrid);
+document.querySelector('#pixelCanvas').addEventListener('click',applyColorToCell);
+document.querySelector('#colorPicker').addEventListener('input',selectColorFromColorPicker);
